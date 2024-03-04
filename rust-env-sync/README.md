f385f5a114a3ac2dbf28385a5af75ff84137c1ed
726a67 <726a67@gmail.com>
2024-03-04 09:50:23 -0600

    functional rust-env-sync

--- rust-env-sync/README.md
+++ rust-env-sync/README.md
@@ -11,3 +11,12 @@ podman run \
 --volume ./root/projects:/projects \
 --rm \
 localhost/rust-env-sync
+
+# run (cache)
+podman run \
+--name rust-env-sync \
+--volume /export/rust:/export \
+--volume ./root/projects:/projects \
+--volume ./root/.cargo/config.toml:/root/.cargo/config.toml \
+--rm \
+localhost/rust-env-sync
--- rust-env-sync/root/usr/local/bin/rust-env-sync.bash
+++ rust-env-sync/root/usr/local/bin/rust-env-sync.bash
@@ -8,18 +8,16 @@ if [ ! -d ${EXPORT_DIR} ]; then echo "[ERROR] Export directory (${EXPORT_DIR}) i
 
 for PROJECT in `ls ${PROJECT_DIR}`
 do
-	REQUIREMENTS_FILE="${PROJECT_DIR}/${PROJECT}/requirements.txt"
+	REQUIREMENTS_FILE="${PROJECT_DIR}/${PROJECT}/Cargo.toml"
 
         echo "[${PROJECT}]" && \
 	if [ ! -f ${REQUIREMENTS_FILE} ]; then echo -e; echo "[ERROR] Requirements file (${REQUIREMENTS_FILE}) is missing"; echo -e; continue; fi && \
         echo -e && \
 	mkdir -p ${EXPORT_DIR}/${PROJECT} && \
 	cd ${EXPORT_DIR}/${PROJECT} && \
-
-	for EXTENSION in $(grep -v "#" ${REQUIREMENTS_FILE})
-	do
-		ovsx get ${EXTENSION} && \
-		echo -e ||
-		echo -e
-	done
+	if [ ! -d ${EXPORT_DIR}/${PROJECT}/src ]; then mkdir ${EXPORT_DIR}/${PROJECT}/src; fi && \
+	if [ ! -f ${EXPORT_DIR}/${PROJECT}/src/lib.rs ]; then echo "// dummy file" > src/lib.rs; fi && \
+	cat ${REQUIREMENTS_FILE} > Cargo.toml && \
+	cargo build && \
+	echo -e
 done
